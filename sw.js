8084b77ad025725bc9bc01d2c1372caed9fe8637
ICIROJAS <113403918+ICIROJAS@users.noreply.github.com>
2025-02-12 12:06:28 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open("app-cache").then(function (cache) {
+            return cache.addAll([
+                "index.html",
+                "script.js",
+                "styles.css",
+                "manifest.json",
+                "icons/icon.png",           
+                "icons/icon-large.png"      
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
